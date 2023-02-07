a42e355eb28236861df44666e5817b845c71aa0f
spdst <124304252+spdst@users.noreply.github.com>
2023-02-06 21:55:29 -0500

    Add simplification

--- solve.py
+++ solve.py
@@ -90,8 +90,7 @@ def mod(x, y): return x % y
 # gets function, pretty simple
 def get_func(f, x): return lambda v: f(v, x)
 
-def solve(q):
-    '''duh'''
+def parse_query(q):
     q = q.lower()
     xls = re.findall('([0123456789i.]+)?x(\^[0123456789]+)?', q)
     xls.sort(key=lambda x: 10000-len(x))
@@ -117,7 +116,8 @@ def solve(q):
     q = q.replace('true', 'B(True)')
     q = q.replace('false', 'B(False)')
     q = q.replace('=', '==')
-    q = '(' + q + ').solve()'
+
+def evaluate(q)
     try:
         return str(eval(q)).replace('True', 'true').replace('False', 'false')
     except decimal.Overflow:
@@ -131,7 +131,7 @@ def solve(q):
     except SyntaxError:
         return 'There is a semantic error in your equation, or you are trying non-natural powers of x'
     except TypeError:
-        return 'There is something wrong with either your equation or XS, or you are trying to ' + \
+        return 'There is something wrong with either your equation or the solver, or you are trying to ' + \
                'solve inequalites, which are not supported :(. Try changing your equation slightly. ' + \
                'If it still dosen\'t work, report it, but if you are trying non-natural powers of x,' + \
                ' those don\'t work either. Or maybe something else is doing this, the error is''' + str(e)
@@ -139,5 +139,9 @@ def solve(q):
         return 'XS has encountered an unspecified error, please report this, error is:' + str(e)
 
 if __name__ = __main__:
-    print(process_query(input('Enter query: ')))
+    query = input('Enter query')
+    query = parse_query(query)
+    print('Solved: ' + evaluate('(' + q + ').solve()'))
+    print('Simplified:' + evaluate(q))
+          
    
